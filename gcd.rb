33c0a8819c58a761cc0167f3e40e18e021ebfb69
Myles Moylan <moylanm@oregonstate.edu>
2022-11-30 13:29:23 -0800

    for discrete math studies

---
+++ gcd.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+def gcd(a, b)
+  x = a
+  y = b
+
+  until y.zero?
+    puts "gcd(#{x}, #{y})"
+    r = x % y
+    x = y
+    y = r
+  end
+  puts "gcd(#{x}, #{y})"
+
+  x
+end
+
+if $PROGRAM_NAME == __FILE__
+
+  options = {}
+  OptionParser.new do |parser|
+    parser.banner = 'Usage: gcd.rb [options]'
+
+    parser.on_tail('-h', '--help', 'Show this message.') do
+      puts parser
+      exit
+    end
+
+    parser.on('-n', '--numbers NUMBERS', Array) do |numbers|
+      options[:numbers] = numbers
+    end
+  end.parse!
+
+  raise OptionParser::MissingArgument if options[:numbers].nil?
+
+  puts gcd(options[:numbers][0].to_i, options[:numbers][1].to_i)
+end
